c5afa2a86ff1751a437353b764cf87c782436309
Santiago Rodrigues <santiagosrod@outlook.com>
2026-02-13 10:38:11 -0300

    feat: Inicializado projeto React com Vite

---
+++ ProjetoPDF/projeto-pdf/src/Home/home.jsx
@@ -0,0 +1,14 @@
+import './home.css'
+
+function Home() {
+
+  return (
+    <>
+      <div style={{background:'white', height:'80vh', width:'150vh', borderRadius:'25px', color:'black'}}>
+        
+      </div>
+    </>
+  )
+}
+
+export default Home
